67c852ef368d95a3cd8be42a5fa5365310d44128
danielcarton <72131582+danielcarton@users.noreply.github.com>
2023-08-10 15:47:21 +0200

    Project plan

--- Code/Energy_subsystem/main/RTC_control.c
+++ Code/Energy_subsystem/main/RTC_control.c
@@ -4,3 +4,22 @@
  * Created: 12/05/2023 22:45:39
  *  Author: danie
  */ 
+
+#include "RTC_control.h"
+
+// This will start a 1kHz clock, which will raise an interrupt every millisecond.
+void RTC_init(){
+    cli();
+    
+    // Set the RTC clock to 1kHz
+    RTC.CLKSEL = RTC_CLKSEL_INT1K_gc;
+    
+    // Enable the RTC
+    RTC.CTRLA = RTC_RTCEN_bm;
+    
+    // Enable the overflow interrupt
+    RTC.INTCTRL = RTC_OVF_bm;
+    
+    // Enable interrupts
+    sei();
+}
--- Code/Energy_subsystem/main/main.c
+++ Code/Energy_subsystem/main/main.c
@@ -17,8 +17,8 @@ int main(void)
     SPI_init();
 	UART_init();
     RTC_init();
-    
-    
+    interrupt_init();
+
 }
 
 void uC_init(void)
@@ -29,5 +29,14 @@ void uC_init(void)
     CLKCTRL.MCLKCTRLB = 0x00;
     CLKCTRL.MCLKCTRLC = 0x00;
     
-    
+
+}
+
+void interrupt_init(void)
+{
+    cli();
+    // 1kHz clock interrupt
+    RTC.INTCTRL = RTC_OVF_bm;
+    sei();
+
 }
